c88b74a3459d86a8fa80409f80d8b0a63c708681
Marcelo Borges <borgespereira.marcelo@gmail.com>
2016-10-10 15:42:38 -0300

    Remove mask and fix indentation

--- assets/js/controllers/main.js
+++ assets/js/controllers/main.js
@@ -1,30 +1,31 @@
 angular.module('taxes', [])
 .controller('main', function($scope, $http) {
-	$http.get('https://app-dot-contabilizei-jobs.appspot.com/rest/simulador/atividades')
-		.success(function(response) {
-			$scope.activities = response.objects;
-		})
-	.error(function(data, status) {
-		alert("Erro ao buscar as atividades.");
-	});
+  $http.get('https://app-dot-contabilizei-jobs.appspot.com/rest/simulador/atividades')
+    .success(function(response) {
+      $scope.atividades = response.objects;
+    })
+    .error(function(data, status) {
+      alert("Erro ao buscar as atividades.");
+    });
 
-	$scope.simulate = function (event, form) {
-		event.preventDefault();
-		calculate($http, $scope, form);
-	}
-
-	$scope.$watchCollection('form.tipo', loadMasks);
+  $scope.simulate = function (event, form) {
+    event.preventDefault();
+    calculate($http, $scope, form);
+  }
 });
 
 
 
 function calculate($http, $scope, form) {
-	$http.get('https://app-dot-contabilizei-jobs.appspot.com/rest/simulador/imposto/'+form.tipo+'?faturamento='+form.faturamento
-	+'&folha='+form.folha+(form.fatanterior ? '&fatanterior='+form.fatanterior : '')+(form.activity ? '&codatividade='+form.activity.cod : ''))
-	.success(function(response) {
-		$scope.result = response;
-	})
-	.error(function(response, status) {
-		alert("Erro ao buscar imposto de lucro presumido.");
-	});
+  $http.get('https://app-dot-contabilizei-jobs.appspot.com/rest/simulador/imposto/'+form.tipo
+  +'?faturamento='+form.faturamento
+  +'&folha='+form.folha
+  +(form.fatanterior ? '&fatanterior='+form.fatanterior : '')
+  +(form.atividade ? '&codatividade='+form.atividade.cod : ''))
+    .success(function(response) {
+      $scope.result = response;
+    })
+    .error(function(response, status) {
+      alert("Erro ao buscar imposto.");
+    });
 }
